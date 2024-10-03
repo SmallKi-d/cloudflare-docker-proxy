e2f20e146f51bbc3e8570d010b56eb55f6b448a2
迷失的小K <1502598781@qq.com>
2024-10-03 16:37:27 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.kclub.tech": dockerHub,
+  "quay.kclub.tech": "https://quay.io",
+  "gcr.kclub.tech": "https://gcr.io",
+  "k8s-gcr.kclub.tech": "https://k8s.gcr.io",
+  "k8s.kclub.tech": "https://registry.k8s.io",
+  "ghcr.kclub.tech": "https://ghcr.io",
+  "cloudsmith.kclub.tech": "https://docker.cloudsmith.io",
+  "ecr.kclub.tech": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.kclub.tech": dockerHub,
 };
 
 function routeByHosts(host) {
